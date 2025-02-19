574360aa5f2e2aa3aa7285056eb7f6da5e392637
RainbowPigCake <kaitlynwong97@gmail.com>
2025-02-18 22:23:52 -0500

    rough draft

---
+++ kaitlyn-personal/app/page.tsx
@@ -0,0 +1,38 @@
+import { Link } from "@heroui/link";
+import { Snippet } from "@heroui/snippet";
+import { Code } from "@heroui/code";
+import { button as buttonStyles } from "@heroui/theme";
+import { Counter } from "@/components/counter";
+import { siteConfig } from "@/config/site";
+import { title, subtitle } from "@/components/primitives";
+import { GithubIcon, LinkedinIcon } from "@/components/icons";
+
+export default function Home() {
+  return (
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <div className="inline-block max-w-xl text-center justify-center">
+        <span className={title()}>Hi! I&apos;m&nbsp;</span>
+        <span className={title({ color: "pink" })}>Kaitlyn</span>
+        <span className={title()}>!</span>
+      </div>
+      <div className="flex gap-3">
+        <Link
+          isExternal
+          className={buttonStyles({ variant: "bordered", radius: "full" })}
+          href={siteConfig.links.github}
+        >
+          <GithubIcon size={26} />
+          GitHub
+        </Link>
+        <Link
+          isExternal
+          className={buttonStyles({ variant: "bordered", radius: "full" })}
+          href={siteConfig.links.linkedin}
+        >
+          <LinkedinIcon size={20} />
+          LinkedIn
+        </Link>
+      </div>
+    </section>
+  );
+}
---
+++ kaitlyn-personal/config/site.ts
@@ -0,0 +1,47 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Kaitlyn Wong",
+  description:
+    "Hello there! My name is Kaitlyn and this is my personal website!",
+  navItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "Experience",
+      href: "/experience",
+    },
+    {
+      label: "Projects",
+      href: "/projects",
+    },
+    {
+      label: "Contact",
+      href: "/contact",
+    },
+  ],
+  navMenuItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "Experience",
+      href: "/experience",
+    },
+    {
+      label: "Projects",
+      href: "/projects",
+    },
+    {
+      label: "Contact",
+      href: "/contact",
+    },
+  ],
+  links: {
+    github: "https://github.com/RainbowPigCake/",
+    linkedin: "https://www.linkedin.com/in/kaitlynwongg/",
+  },
+};
